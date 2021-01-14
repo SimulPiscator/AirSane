1c9fb412298ab3f6b0100f285b6611dce6862b4c
Simul Piscator <simul.piscator@github.com>
2021-01-14 21:16:55 +0100

    clang-format -style=Mozilla

--- zeroconf/mdnspublisher-dnssd.mm
+++ zeroconf/mdnspublisher-dnssd.mm
@@ -18,40 +18,44 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "mdnspublisher.h"
 
+#include <ctime>
 #include <iostream>
 #include <list>
-#include <ctime>
 
-#include <unistd.h>
-#include <netdb.h>
-#include <dns_sd.h>
 #include <Foundation/Foundation.h>
+#include <dns_sd.h>
+#include <netdb.h>
+#include <unistd.h>
 
 namespace {
 
-static struct { int code; const char* text; }
-sErrors[] = {
+static struct
+{
+  int code;
+  const char* text;
+} sErrors[] = {
 #define _(x) { x, #x },
-_(kDNSServiceErr_NoError)//                    = 0,
-_(kDNSServiceErr_Unknown)//                    = -65537,    // 0xFFFE FFFF (first error code)
-_(kDNSServiceErr_NoSuchName)//                = -65538,
-_(kDNSServiceErr_NoMemory)//                    = -65539,
-_(kDNSServiceErr_BadParam)//                    = -65540,
-_(kDNSServiceErr_BadReference)//                = -65541,
-_(kDNSServiceErr_BadState)//                    = -65542,
-_(kDNSServiceErr_BadFlags)//                    = -65543,
-_(kDNSServiceErr_Unsupported)//                = -65544,
-_(kDNSServiceErr_NotInitialized)//            = -65545,
-//    _(kDNSServiceErr_NoCache)//                    = -65546,
-_(kDNSServiceErr_AlreadyRegistered)//        = -65547,
-_(kDNSServiceErr_NameConflict)//                = -65548,
-_(kDNSServiceErr_Invalid)//                    = -65549,
-_(kDNSServiceErr_Incompatible)//                = -65551,
-_(kDNSServiceErr_BadInterfaceIndex)//        = -65552,
-_(kDNSServiceErr_Refused)//                    = -65553,
-_(kDNSServiceErr_NoSuchRecord)//                = -65554,
-_(kDNSServiceErr_NoAuth)//                    = -65555,
-_(kDNSServiceErr_NoSuchKey)//                = -65556,
+  _(kDNSServiceErr_NoError)    //                    = 0,
+  _(kDNSServiceErr_Unknown)    //                    = -65537,    // 0xFFFE FFFF
+                               //                    (first error code)
+  _(kDNSServiceErr_NoSuchName) //                = -65538,
+  _(kDNSServiceErr_NoMemory)   //                    = -65539,
+  _(kDNSServiceErr_BadParam)   //                    = -65540,
+  _(kDNSServiceErr_BadReference)   //                = -65541,
+  _(kDNSServiceErr_BadState)       //                    = -65542,
+  _(kDNSServiceErr_BadFlags)       //                    = -65543,
+  _(kDNSServiceErr_Unsupported)    //                = -65544,
+  _(kDNSServiceErr_NotInitialized) //            = -65545,
+  //    _(kDNSServiceErr_NoCache)//                    = -65546,
+  _(kDNSServiceErr_AlreadyRegistered) //        = -65547,
+  _(kDNSServiceErr_NameConflict)      //                = -65548,
+  _(kDNSServiceErr_Invalid)           //                    = -65549,
+  _(kDNSServiceErr_Incompatible)      //                = -65551,
+  _(kDNSServiceErr_BadInterfaceIndex) //        = -65552,
+  _(kDNSServiceErr_Refused)           //                    = -65553,
+  _(kDNSServiceErr_NoSuchRecord)      //                = -65554,
+  _(kDNSServiceErr_NoAuth)            //                    = -65555,
+  _(kDNSServiceErr_NoSuchKey)         //                = -65556,
 //    _(kDNSServiceErr_NoValue)//                    = -65557,
 //    _(kDNSServiceErr_BufferTooSmall)//            = -65558,
 
@@ -65,155 +69,181 @@ _(kDNSServiceErr_NoSuchKey)//                = -65556,
 
 //    _(kDNSServiceErr_GrowCache)//                = -65790,
 //    _(kDNSServiceErr_ConfigChanged)//            = -65791,
-//    _(kDNSServiceErr_MemFree)//                    = -65792    // 0xFFFE FF00 (last error code)
+//    _(kDNSServiceErr_MemFree)//                    = -65792    // 0xFFFE FF00
+//    (last error code)
 #undef _
-    { 0, nullptr }
+  { 0, nullptr }
 };
-const char* dnssd_strerr(int code)
+const char*
+dnssd_strerr(int code)
 {
-    for (const auto err : sErrors)
-        if (err.code == code)
-            return err.text;
-    return ::strerror(code);
+  for (const auto err : sErrors)
+    if (err.code == code)
+      return err.text;
+  return ::strerror(code);
 }
 
 static int sAddAnnounceDelaySeconds = 1;
 
 struct ServiceEntry
 {
-    MdnsPublisher::Service* mpService;
-    DNSServiceRef mDNSServiceRef;
+  MdnsPublisher::Service* mpService;
+  DNSServiceRef mDNSServiceRef;
 
-    ServiceEntry(MdnsPublisher::Service* p)
-        : mpService(p), mDNSServiceRef(nullptr)
-    {
-    }
+  ServiceEntry(MdnsPublisher::Service* p)
+    : mpService(p)
+    , mDNSServiceRef(nullptr)
+  {}
 
-    ~ServiceEntry()
-    {
-        unannounce();
-    }
+  ~ServiceEntry() { unannounce(); }
 
-    bool announce()
-    {
-        unannounce();
-        bool ok = true;
-        TXTRecordRef txtRecord;
-        ::TXTRecordCreate(&txtRecord, 0, nullptr);
-        for (const auto& entry : mpService->txtRecord()) {
-            DNSServiceErrorType err = ::TXTRecordSetValue(&txtRecord, entry.first.c_str(), entry.second.length(), entry.second.data());
-            if (err != kDNSServiceErr_NoError) {
-                ok = false;
-                std::cerr << "Could not add txtRecord value " << entry.first << "=" << entry.second << " (" << dnssd_strerr(err) << ")" << std::endl;
-            }
-        }
-        int ifindex = mpService->interfaceIndex();
-        if (ifindex < 0)
-            ifindex = 0;
-        uint16_t port = mpService->port();
-        ::usleep(1000 * 1000 * sAddAnnounceDelaySeconds); // without this, service registration may fail but not report an error (synchronization issue?)
-        DNSServiceErrorType err = ::DNSServiceRegister(&mDNSServiceRef, 0, ifindex, mpService->name().c_str(), mpService->type().c_str(), nullptr, nullptr, htons(port), ::TXTRecordGetLength(&txtRecord), ::TXTRecordGetBytesPtr(&txtRecord), onRegisterService, mpService);
-        if (err != kDNSServiceErr_NoError) {
-            ok = false;
-            std::cerr << "Could not register service " << mpService->name() << " (" << dnssd_strerr(err) << ")" << std::endl;
-        }
-        else {
-            int fd = ::DNSServiceRefSockFD(mDNSServiceRef);
-            fd_set readfds;
-            FD_ZERO(&readfds);
-            FD_SET(fd, &readfds);
-            int r = ::select(fd + 1, &readfds, nullptr, nullptr, nullptr);
-            if (r == 1)
-                ::DNSServiceProcessResult(mDNSServiceRef);
-        }
-        ::TXTRecordDeallocate(&txtRecord);
-        return ok;
+  bool announce()
+  {
+    unannounce();
+    bool ok = true;
+    TXTRecordRef txtRecord;
+    ::TXTRecordCreate(&txtRecord, 0, nullptr);
+    for (const auto& entry : mpService->txtRecord()) {
+      DNSServiceErrorType err = ::TXTRecordSetValue(&txtRecord,
+                                                    entry.first.c_str(),
+                                                    entry.second.length(),
+                                                    entry.second.data());
+      if (err != kDNSServiceErr_NoError) {
+        ok = false;
+        std::cerr << "Could not add txtRecord value " << entry.first << "="
+                  << entry.second << " (" << dnssd_strerr(err) << ")"
+                  << std::endl;
+      }
     }
+    int ifindex = mpService->interfaceIndex();
+    if (ifindex < 0)
+      ifindex = 0;
+    uint16_t port = mpService->port();
+    ::usleep(1000 * 1000 *
+             sAddAnnounceDelaySeconds); // without this, service registration
+                                        // may fail but not report an error
+                                        // (synchronization issue?)
+    DNSServiceErrorType err =
+      ::DNSServiceRegister(&mDNSServiceRef,
+                           0,
+                           ifindex,
+                           mpService->name().c_str(),
+                           mpService->type().c_str(),
+                           nullptr,
+                           nullptr,
+                           htons(port),
+                           ::TXTRecordGetLength(&txtRecord),
+                           ::TXTRecordGetBytesPtr(&txtRecord),
+                           onRegisterService,
+                           mpService);
+    if (err != kDNSServiceErr_NoError) {
+      ok = false;
+      std::cerr << "Could not register service " << mpService->name() << " ("
+                << dnssd_strerr(err) << ")" << std::endl;
+    } else {
+      int fd = ::DNSServiceRefSockFD(mDNSServiceRef);
+      fd_set readfds;
+      FD_ZERO(&readfds);
+      FD_SET(fd, &readfds);
+      int r = ::select(fd + 1, &readfds, nullptr, nullptr, nullptr);
+      if (r == 1)
+        ::DNSServiceProcessResult(mDNSServiceRef);
+    }
+    ::TXTRecordDeallocate(&txtRecord);
+    return ok;
+  }
 
-    void unannounce()
-    {
-        if(mDNSServiceRef) {
-            ::DNSServiceRefDeallocate(mDNSServiceRef);
-            mDNSServiceRef = nullptr;
-        }
+  void unannounce()
+  {
+    if (mDNSServiceRef) {
+      ::DNSServiceRefDeallocate(mDNSServiceRef);
+      mDNSServiceRef = nullptr;
     }
+  }
 
-    static void onRegisterService(
-        DNSServiceRef, DNSServiceFlags,
-        DNSServiceErrorType errorCode, const char *name,
-        const char *regType, const char *domain, void *data)
-    {
-        auto* pService = static_cast<MdnsPublisher::Service*>(data);
-        if (errorCode == kDNSServiceErr_NoError)
-            pService->setName(name);
-    }
+  static void onRegisterService(DNSServiceRef,
+                                DNSServiceFlags,
+                                DNSServiceErrorType errorCode,
+                                const char* name,
+                                const char* regType,
+                                const char* domain,
+                                void* data)
+  {
+    auto* pService = static_cast<MdnsPublisher::Service*>(data);
+    if (errorCode == kDNSServiceErr_NoError)
+      pService->setName(name);
+  }
 };
 
 } // namespace
 
 struct MdnsPublisher::Private
 {
-    std::string mHostnameFqdn, mHostname;
-    std::list<ServiceEntry> mServices;
+  std::string mHostnameFqdn, mHostname;
+  std::list<ServiceEntry> mServices;
 
-    std::list<ServiceEntry>::iterator findService(const Service* pService)
-    {
-        auto i = std::find_if(mServices.begin(), mServices.end(),
-            [pService](const ServiceEntry& entry) {
-                return entry.mpService == pService;
-             });
-        return i;
-    }
-
+  std::list<ServiceEntry>::iterator findService(const Service* pService)
+  {
+    auto i = std::find_if(mServices.begin(),
+                          mServices.end(),
+                          [pService](const ServiceEntry& entry) {
+                            return entry.mpService == pService;
+                          });
+    return i;
+  }
 };
 
 MdnsPublisher::MdnsPublisher()
-    : p(new Private)
+  : p(new Private)
 {
-    NSHost* host = [NSHost currentHost];
-    auto names = [host names];
-    if ([names count] > 0)
-        p->mHostnameFqdn = [names[0] UTF8String];
-    size_t pos = p->mHostnameFqdn.find('.');
-    p->mHostname = p->mHostnameFqdn.substr(0, pos);
+  NSHost* host = [NSHost currentHost];
+  auto names = [host names];
+  if ([names count] > 0)
+    p->mHostnameFqdn = [names[0] UTF8String];
+  size_t pos = p->mHostnameFqdn.find('.');
+  p->mHostname = p->mHostnameFqdn.substr(0, pos);
 }
 
 MdnsPublisher::~MdnsPublisher()
 {
-    delete p;
+  delete p;
 }
 
-const std::string& MdnsPublisher::hostname() const
+const std::string&
+MdnsPublisher::hostname() const
 {
-    return p->mHostname;
+  return p->mHostname;
 }
 
-const std::string& MdnsPublisher::hostnameFqdn() const
+const std::string&
+MdnsPublisher::hostnameFqdn() const
 {
-    return p->mHostnameFqdn;
+  return p->mHostnameFqdn;
 }
 
-bool MdnsPublisher::announce(MdnsPublisher::Service *pService)
+bool
+MdnsPublisher::announce(MdnsPublisher::Service* pService)
 {
-    bool ok = false;
-    auto i = p->findService(pService);
-    if (i != p->mServices.end()) {
-        ok = true;
-    } else {
-        p->mServices.push_back(ServiceEntry(pService));
-        ok = p->mServices.back().announce();
-    }
-    return ok;
+  bool ok = false;
+  auto i = p->findService(pService);
+  if (i != p->mServices.end()) {
+    ok = true;
+  } else {
+    p->mServices.push_back(ServiceEntry(pService));
+    ok = p->mServices.back().announce();
+  }
+  return ok;
 }
 
-bool MdnsPublisher::unannounce(MdnsPublisher::Service *pService)
+bool
+MdnsPublisher::unannounce(MdnsPublisher::Service* pService)
 {
-    bool ok = false;
-    auto i = p->findService(pService);
-    if (i != p->mServices.end()) {
-        ok = true;
-        i->unannounce();
-        p->mServices.erase(i);
-    }
-    return ok;
+  bool ok = false;
+  auto i = p->findService(pService);
+  if (i != p->mServices.end()) {
+    ok = true;
+    i->unannounce();
+    p->mServices.erase(i);
+  }
+  return ok;
 }
